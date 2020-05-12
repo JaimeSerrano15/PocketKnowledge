6a56cac355a70ae6667c69e1da62b0ee3dc71ea7
AlejandroPinto99 <00156618@uca.edu.sv>
2020-05-12 00:39:11 -0600

    Adding visual elementos, HomeFragment

--- PocketKnowledgeApp/app/src/main/java/com/jaiser/pocketknowledgeapp/MainActivity.kt
+++ PocketKnowledgeApp/app/src/main/java/com/jaiser/pocketknowledgeapp/MainActivity.kt
@@ -2,11 +2,15 @@ package com.jaiser.pocketknowledgeapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.jaiser.pocketknowledgeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>( this,
+            R.layout.activity_main)
     }
 }
