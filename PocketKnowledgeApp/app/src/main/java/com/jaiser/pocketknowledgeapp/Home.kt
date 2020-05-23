3c96949fb3951e52a0f2d97171aad2a64770a267
JaimeSerrano15 <00025318@uca.edu.sv>
2020-05-22 22:36:07 -0600

    Logos and Navigation

--- PocketKnowledgeApp/app/src/main/java/com/jaiser/pocketknowledgeapp/Home.kt
+++ PocketKnowledgeApp/app/src/main/java/com/jaiser/pocketknowledgeapp/Home.kt
@@ -6,13 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.jaiser.pocketknowledgeapp.databinding.FragmentHomeBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class Home : Fragment() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,11 +26,15 @@ class Home : Fragment() {
         val binding = DataBindingUtil.inflate<FragmentHomeBinding>(inflater,
         R.layout.fragment_home, container, false)
 
+        drawerLayout = binding.drawerLayout
+
         binding.btnMath.setOnClickListener{view: View->
             view.findNavController().navigate(R.id.action_home2_to_levelFragment)
         }
 
         return binding.root
     }
+
+
 
 }
