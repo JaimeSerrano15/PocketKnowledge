38bf60c346a75fc78d75dbefa036c269cd6e9cbc
JaimeSerrano15 <00025318@uca.edu.sv>
2020-07-13 09:54:36 -0600

    License

--- app/src/main/java/com/jaiser/pocketknowledgeapp/EmailFragment.kt
+++ app/src/main/java/com/jaiser/pocketknowledgeapp/EmailFragment.kt
@@ -1,3 +1,22 @@
+/*
+    Pocket Knowledge. Application for educational purposes
+    Copyright (C) 2020. Empresaurios
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact: empresaurios2020@gmail.com
+ */
 package com.jaiser.pocketknowledgeapp
 
 import android.content.Intent
@@ -8,6 +27,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.jaiser.pocketknowledgeapp.databinding.FragmentEmailBinding
 import java.lang.Exception
@@ -28,6 +48,7 @@ class EmailFragment : Fragment() {
             container,
             false
         )
+        setup()
 
         binding.sendButton.setOnClickListener { view ->
             val destination = "empresaurios2020@gmail.com"
@@ -35,6 +56,8 @@ class EmailFragment : Fragment() {
             val message = binding.messageEt.text.toString()
             sendEmail(destination, subject, message, binding)
         }
+
+
 
         return binding.root
     }
@@ -56,6 +79,10 @@ class EmailFragment : Fragment() {
         } catch (e: Exception) {
             Toast.makeText(this.context, e.message, Toast.LENGTH_SHORT).show()
         }
+    }
+
+    fun setup(){
+        (activity as AppCompatActivity).supportActionBar?.title = "Comentarios"
     }
 
 }
